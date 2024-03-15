34abe0b3ab479273c25041096d8b67e13fee3e01
ziyadanas <ziyadanas125@gmail.com>
2024-03-15 23:54:25 +0800

    add music policy

---
+++ js/audio_policy.js
@@ -0,0 +1,9 @@
+function acceptPolicy() {
+    document.getElementById("policy").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+  
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+}
