71fcfaa3ee0fb54dcbefa6a507b20be90c8dfc58
ziyadanas <ziyadanas125@gmail.com>
2025-10-03 21:07:46 +0800

    delete older files

--- js/audio_policy.js
+++
@@ -1,9 +0,0 @@
-function acceptPolicy() {
-    document.getElementById("policy").style.display = "none";
-    document.getElementById("content").style.display = "block";
-}
-  
-function playAudio() {
-    var audio = document.getElementById("myAudio");
-    audio.play();
-}
